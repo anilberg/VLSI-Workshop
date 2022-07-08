d3fea34f263f4f836c0d297a58b07fc292311f83
unknown <anilberk.atas@ogr.iuc.edu.tr>
2022-07-08 21:18:52 +0300

    Update ripple_carry
    WIDTH parameter added to change operation bit size.
    
    full_adder file deleted, merged with ripple_carry module.

--- Adders/src/ripple_carry.v
+++ Adders/src/ripple_carry.v
@@ -14,18 +14,37 @@
 //   	limitations under the License.
 //
 // 	author		: 	@anilberg
-//		project		:	VLSI Wokshop
+//		project		:	VLSI Wokshop - Adders
 // 	module		:	Ripple Carry Adder Module
 // 	Description	: 	
 //
-// 	Date			:	7 JUL 2022
-// 	Version		:	1.0
+// 	Date			:	8 JUL 2022
+// 	Version		:	1.1
 // -------------------------------------------------------------------------------
 
 `timescale 10ns/1ps
-// ------------------------------- Includes --------------------------------------
-`include "../src/full_adder.v"
+// -------------------------------------------------------------------------------
+// ------------------------- Full Adder Module -----------------------------------
+module full_adder
+(					//Inputs
+					input 		A_i,
+					input 		B_i,
+					input			C_i,
+					//Outputs
+					//output reg	S_o,
+					//output reg	C_o
+					output	S_o,
+					output	C_o
+);
 
+// --------------------------- Define Outputs ------------------------------------
+assign S_o = A_i ^ B_i ^ C_i;
+assign C_o = ((A_i ^ B_i) & C_i) | (A_i & B_i);
+
+endmodule
+// ----------------------- Full Adder Module End ---------------------------------
+
+// -------------------------------------------------------------------------------
 // ----------------------- Ripple Carry Adder Module -----------------------------
 module ripple_carry
 #(
@@ -40,332 +59,47 @@ module ripple_carry
 					output 			 		Carry_o
 );
 
-// --------------------------------- Wires ---------------------------------------
-wire c1,  c2,  c3,  c4,  c5,  c6,  c7,  c8,
-	  c9,	 c10, c11, c12, c13, c14, c15, c16,
-	  c17, c18, c19, c20, c21, c22, c23, c24,
-	  c25, c26, c27, c28, c29, c30, c31;
+//To keep carry outs for each FA module
+wire [WIDTH-2:0] c_o;
 
-// ------------------------------- Submodules ------------------------------------
-full_adder full_adder0
-(						//Inputs
-						.A_i(Number1_i[0]),
-						.B_i(Number2_i[0]),
-						.C_i(Carry_i),
-						//Outputs
-						.S_o(Result_o[0]),
-						.C_o(c1)
+//First Full Adder Block
+full_adder fa0
+(				//Inputs
+				.A_i(Number1_i[0]),
+				.B_i(Number2_i[0]),
+				.C_i(Carry_i),
+				//Outputs
+				.S_o(Result_o[0]),
+				.C_o(c_o[0])
 );
 
-full_adder full_adder1
-(						//Inputs
-						.A_i(Number1_i[1]),
-						.B_i(Number2_i[1]),
-						.C_i(c1),
-						//Outputs
-						.S_o(Result_o[1]),
-						.C_o(c2)
+//Last Full Adder Block
+full_adder fa_last
+(				//Inputs
+				.A_i(Number1_i[WIDTH-1]),
+				.B_i(Number2_i[WIDTH-1]),
+				.C_i(c_o[WIDTH-2]),
+				//Outputs
+				.S_o(Result_o[WIDTH-1]),
+				.C_o(Carry_o)
 );
 
-full_adder full_adder2
-(						//Inputs
-						.A_i(Number1_i[2]),
-						.B_i(Number2_i[2]),
-						.C_i(c2),
-						//Outputs
-						.S_o(Result_o[2]),
-						.C_o(c3)
-);
+//Generate Other Full Adder Blocks
+generate
+	genvar j;
 
-full_adder full_adder3
-(						//Inputs
-						.A_i(Number1_i[3]),
-						.B_i(Number2_i[3]),
-						.C_i(c3),
+	for (j = 1; j < WIDTH - 1; j = j + 1) begin :fa
+		full_adder fa
+		(				//Inputs
+						.A_i(Number1_i[j]),
+						.B_i(Number2_i[j]),
+						.C_i(c_o[j-1]),
 						//Outputs
-						.S_o(Result_o[3]),
-						.C_o(c4)
-);
-
-full_adder full_adder4
-(						//Inputs
-						.A_i(Number1_i[4]),
-						.B_i(Number2_i[4]),
-						.C_i(c4),
-						//Outputs
-						.S_o(Result_o[4]),
-						.C_o(c5)
-);
-
-full_adder full_adder5
-(						//Inputs
-						.A_i(Number1_i[5]),
-						.B_i(Number2_i[5]),
-						.C_i(c5),
-						//Outputs
-						.S_o(Result_o[5]),
-						.C_o(c6)
-);
-
-full_adder full_adder6
-(						//Inputs
-						.A_i(Number1_i[6]),
-						.B_i(Number2_i[6]),
-						.C_i(c6),
-						//Outputs
-						.S_o(Result_o[6]),
-						.C_o(c7)
-);
-
-full_adder full_adder7
-(						//Inputs
-						.A_i(Number1_i[7]),
-						.B_i(Number2_i[7]),
-						.C_i(c7),
-						//Outputs
-						.S_o(Result_o[7]),
-						.C_o(c8)
-);
-
-full_adder full_adder8
-(						//Inputs
-						.A_i(Number1_i[8]),
-						.B_i(Number2_i[8]),
-						.C_i(c8),
-						//Outputs
-						.S_o(Result_o[8]),
-						.C_o(c9)
-);
-
-full_adder full_adder9
-(						//Inputs
-						.A_i(Number1_i[9]),
-						.B_i(Number2_i[9]),
-						.C_i(c9),
-						//Outputs
-						.S_o(Result_o[9]),
-						.C_o(c10)
-);
-
-full_adder full_adder10
-(						//Inputs
-						.A_i(Number1_i[10]),
-						.B_i(Number2_i[10]),
-						.C_i(c10),
-						//Outputs
-						.S_o(Result_o[10]),
-						.C_o(c11)
-);
-
-full_adder full_adder11
-(						//Inputs
-						.A_i(Number1_i[11]),
-						.B_i(Number2_i[11]),
-						.C_i(c11),
-						//Outputs
-						.S_o(Result_o[11]),
-						.C_o(c12)
-);
-
-full_adder full_adder12
-(						//Inputs
-						.A_i(Number1_i[12]),
-						.B_i(Number2_i[12]),
-						.C_i(c12),
-						//Outputs
-						.S_o(Result_o[12]),
-						.C_o(c13)
-);
-
-full_adder full_adder13
-(						//Inputs
-						.A_i(Number1_i[13]),
-						.B_i(Number2_i[13]),
-						.C_i(c13),
-						//Outputs
-						.S_o(Result_o[13]),
-						.C_o(c14)
-);
-
-full_adder full_adder14
-(						//Inputs
-						.A_i(Number1_i[14]),
-						.B_i(Number2_i[14]),
-						.C_i(c14),
-						//Outputs
-						.S_o(Result_o[14]),
-						.C_o(c15)
-);
-
-full_adder full_adder15
-(						//Inputs
-						.A_i(Number1_i[15]),
-						.B_i(Number2_i[15]),
-						.C_i(c15),
-						//Outputs
-						.S_o(Result_o[15]),
-						.C_o(c16)
-);
-
-full_adder full_adder16
-(						//Inputs
-						.A_i(Number1_i[16]),
-						.B_i(Number2_i[16]),
-						.C_i(c16),
-						//Outputs
-						.S_o(Result_o[16]),
-						.C_o(c17)
-);
-
-full_adder full_adder17
-(						//Inputs
-						.A_i(Number1_i[17]),
-						.B_i(Number2_i[17]),
-						.C_i(c17),
-						//Outputs
-						.S_o(Result_o[17]),
-						.C_o(c18)
-);
-
-full_adder full_adder18
-(						//Inputs
-						.A_i(Number1_i[18]),
-						.B_i(Number2_i[18]),
-						.C_i(c18),
-						//Outputs
-						.S_o(Result_o[18]),
-						.C_o(c19)
-);
-
-full_adder full_adder19
-(						//Inputs
-						.A_i(Number1_i[19]),
-						.B_i(Number2_i[19]),
-						.C_i(c19),
-						//Outputs
-						.S_o(Result_o[19]),
-						.C_o(c20)
-);
-
-full_adder full_adder20
-(						//Inputs
-						.A_i(Number1_i[20]),
-						.B_i(Number2_i[20]),
-						.C_i(c20),
-						//Outputs
-						.S_o(Result_o[20]),
-						.C_o(c21)
-);
-
-full_adder full_adder21
-(						//Inputs
-						.A_i(Number1_i[21]),
-						.B_i(Number2_i[21]),
-						.C_i(c21),
-						//Outputs
-						.S_o(Result_o[21]),
-						.C_o(c22)
-);
-
-full_adder full_adder22
-(						//Inputs
-						.A_i(Number1_i[22]),
-						.B_i(Number2_i[22]),
-						.C_i(c22),
-						//Outputs
-						.S_o(Result_o[22]),
-						.C_o(c23)
-);
-
-full_adder full_adder23
-(						//Inputs
-						.A_i(Number1_i[23]),
-						.B_i(Number2_i[23]),
-						.C_i(c23),
-						//Outputs
-						.S_o(Result_o[23]),
-						.C_o(c24)
-);
-
-full_adder full_adder24
-(						//Inputs
-						.A_i(Number1_i[24]),
-						.B_i(Number2_i[24]),
-						.C_i(c24),
-						//Outputs
-						.S_o(Result_o[24]),
-						.C_o(c25)
-);
-
-full_adder full_adder25
-(						//Inputs
-						.A_i(Number1_i[25]),
-						.B_i(Number2_i[25]),
-						.C_i(c25),
-						//Outputs
-						.S_o(Result_o[25]),
-						.C_o(c26)
-);
-
-full_adder full_adder26
-(						//Inputs
-						.A_i(Number1_i[26]),
-						.B_i(Number2_i[26]),
-						.C_i(c26),
-						//Outputs
-						.S_o(Result_o[26]),
-						.C_o(c27)
-);
-
-full_adder full_adder27
-(						//Inputs
-						.A_i(Number1_i[27]),
-						.B_i(Number2_i[27]),
-						.C_i(c27),
-						//Outputs
-						.S_o(Result_o[27]),
-						.C_o(c28)
-);
-
-full_adder full_adder28
-(						//Inputs
-						.A_i(Number1_i[28]),
-						.B_i(Number2_i[28]),
-						.C_i(c28),
-						//Outputs
-						.S_o(Result_o[28]),
-						.C_o(c29)
-);
-
-full_adder full_adder29
-(						//Inputs
-						.A_i(Number1_i[29]),
-						.B_i(Number2_i[29]),
-						.C_i(c29),
-						//Outputs
-						.S_o(Result_o[29]),
-						.C_o(c30)
-);
-
-full_adder full_adder30
-(						//Inputs
-						.A_i(Number1_i[30]),
-						.B_i(Number2_i[30]),
-						.C_i(c30),
-						//Outputs
-						.S_o(Result_o[30]),
-						.C_o(c31)
-);
-
-full_adder full_adder31
-(						//Inputs
-						.A_i(Number1_i[31]),
-						.B_i(Number2_i[31]),
-						.C_i(c31),
-						//Outputs
-						.S_o(Result_o[31]),
-						.C_o(Carry_o)
-);
+						.S_o(Result_o[j]),
+						.C_o(c_o[j])
+		);
+	end
+endgenerate
 
 endmodule
 // --------------------- Ripple Carry Adder Module End ---------------------------
